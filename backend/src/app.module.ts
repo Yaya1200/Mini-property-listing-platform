7efeb7173c70de796e596b393d7f791b5495bb95
yaredgebre <yaredgebre120@gmail.com>
2026-09-14 06:54:56 -0300

    adding admin module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module.js';
 import { PropertiesModule } from './properties/properties.module.js';
 import { FavoritesModule } from './favorites/favorites.module.js';
+import { AdminModule } from './admin/admin.module.js'; 
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
@@ -15,6 +16,7 @@ import { AppService } from './app.service.js';
     AuthModule,
     PropertiesModule,
     FavoritesModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
